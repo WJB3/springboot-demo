481d980a0c1d8d23e931aeccfbb8dc2caa85248c
wujb <wujb@tranwin.org>
2020-01-10 19:42:08 +0800

    demo初始化

--- src/main/java/com/example/demo/model/UserInfo.java
+++ src/main/java/com/example/demo/model/UserInfo.java
@@ -15,5 +15,20 @@ public class UserInfo {
     public  String getId(){
         return id;
     }
-    
+    public void setId(String id){
+        this.id=id;
+    }
+    public String getUserName(){
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
 }
