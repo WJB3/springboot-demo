d1f192ba8806ff371cc72a32b2b84b76c3e75a18
wujb <wujb@tranwin.org>
2020-01-10 18:54:31 +0800

    sbdemo学习

---
+++ src/main/java/com/example/demo/core/configurer/MybatisConfigurer.java
@@ -0,0 +1,4 @@
+package com.example.demo.core.configurer;
+
+public class MybatisConfigurer {
+}
---
+++ src/main/java/com/example/demo/model/UserInfo.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+public class UserInfo {
+
+    /**
+     * 主键
+     */
+    private String id;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+    private String password;
+    public  String getId(){
+        return id;
+    }
+    
+}
